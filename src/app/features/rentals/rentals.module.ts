e5842c1ea52792757498699d51e7e5a9eae6e759
Gregoire Cattan <gregoire.cattan@ibm.com>
2025-07-05 23:24:43 +0200

    migrate to ngx-custom-material-file-input

--- src/app/features/rentals/rentals.module.ts
+++ src/app/features/rentals/rentals.module.ts
@@ -15,7 +15,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MaterialFileInputModule } from 'ngx-material-file-input';
+import { MaterialFileInputModule } from 'ngx-custom-material-file-input';
 registerLocaleData(localeFr);
 
 const materialModules = [
